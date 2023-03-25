1df81beb4728f74886796c1cc49920d9b1c5063f
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-03-25 13:01:18 +0100

    done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,14 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> map = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            map.put(entry.getValue(), entry.getKey());
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] arr = sentence.toLowerCase().split(" ");
+        for (String s : arr) {
+            if (map.containsKey(s)) {
+                int count = map.get(s);
+                map.put(s, count + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+        return map;
     }
 }
